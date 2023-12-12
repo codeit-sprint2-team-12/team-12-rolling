35ba1c89804190989eb59685053d7380a2dc4f2d
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-12-12 14:43:22 +0900

    style: Badge 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import GlobalStyle from './components/GlobalStyle';
-import Toast from './components/Toast/Toast';
-
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Toast />
     </div>
   );
 }
