32dbbe1c3cea9bc99cb8faf773d2366642af85e7
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-12-12 12:16:38 +0900

    feat: GlobalStyled 지정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import GlobalStyle from './components/GlobalStyle';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/GlobalStyle.jsx
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+html {
+ font-size: 62.5%;
+ font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+}
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+`;
+
+export default GlobalStyle;
