ac246830808ff4ccff649e67eace83cdeec3d1d3
Sungeunji <danceunji@gmail.com>
2023-12-13 16:44:41 +0900

    chore: Dropdown컴포넌트의  이미지파일2개  추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import GlobalStyle from './components/GlobalStyle';
+import Dropdown from './components/TextField/Dropdown';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Dropdown />
+    </div>
+  );
 }
 
 export default App;
