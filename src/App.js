cb81fb7d19bee24e963c8751fd291faa9871d8c8
Sungeunji <danceunji@gmail.com>
2023-12-13 16:48:38 +0900

    chore: pr실험

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import GlobalStyle from './components/GlobalStyle';
-import Dropdown from './components/TextField/Dropdown';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Dropdown />
     </div>
   );
 }
