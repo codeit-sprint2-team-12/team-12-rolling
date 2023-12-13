e43ab8aa401168e8c1ec4fec0a19a2da273b35e5
박지원 <jackgg12322@gmail.com>
2023-12-13 16:02:11 +0900

    feat : 버튼 컴포넌트 구현

---
+++ src/components/Button/SecondaryBtn.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const SecondaryBtn = ({ children, disabled }) => {
+  return <CommonButtonStyle disabled={disabled}>{children}</CommonButtonStyle>;
+};
+
+const CommonButtonStyle = styled.button`
+  display: inline-flex;
+  padding: 0.7rem 1.6rem;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  border-radius: 0.6rem;
+  color: var(--purple-600, #9935ff);
+  background: var(--white, #fff);
+  border: 0.1rem solid var(--purple-600, #9935ff);
+
+  &:hover {
+    border: 0.1rem solid var(--purple-700, #861dee);
+    background: var(--purple-100, #f8f0ff);
+  }
+  &:active {
+    border: 0.1rem solid var(--purple-600, #9935ff);
+    background: var(--white, #fff);
+  }
+
+  &:focus {
+    border: 0.1rem solid var(--purple-800, #6e0ad1);
+    background: var(--white, #fff);
+  }
+
+  &:disabled {
+    color: var(--white, #fff);
+    background: var(--gray-300, #ccc);
+    border: none;
+  }
+`;
+
+export default SecondaryBtn;
