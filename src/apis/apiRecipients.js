2fa353a29b818c62843292a56d6a9202e42bcb0a
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-12-14 12:10:21 +0900

    feat: recipient관련  GET 요청함수 구현

--- src/apis/apiRecipients.js
+++ src/apis/apiRecipients.js
@@ -3,8 +3,36 @@ const ROOT_URL = 'https://rolling-api.vercel.app/2-12';
 /* ==================== */
 // GET
 /* ==================== */
-async function getRecipients() {
-  const response = await fetch(`${ROOT_URL}/recipients/`);
+
+async function getRecipients(id = '') {
+  const path = id ? `recipients/${id}/` : `recipients/`;
+  const response = await fetch(`${ROOT_URL}/${path}`);
+  if (!response.ok) {
+    throw new Error('대상을 불러오는데 실패했습니다.');
+  }
+  const body = await response.json();
+
+  return body;
+}
+
+async function getRecipientMessages(id = '') {
+  if (!id) {
+    throw new Error('불러올 대상을 지정해주세요.');
+  }
+  const response = await fetch(`${ROOT_URL}/recipients/${id}/messages/`);
+  if (!response.ok) {
+    throw new Error('대상을 불러오는데 실패했습니다.');
+  }
+  const body = await response.json();
+
+  return body;
+}
+
+async function getRecipientReactions(id = '') {
+  if (!id) {
+    throw new Error('불러올 대상을 지정해주세요.');
+  }
+  const response = await fetch(`${ROOT_URL}/recipients/${id}/reactions/`);
   if (!response.ok) {
     throw new Error('대상을 불러오는데 실패했습니다.');
   }
@@ -30,4 +58,19 @@ async function postRecipients(data) {
   const body = await response.json();
 
   return body;
+}
+
+/* ==================== */
+// DELETE
+/* ==================== */
+
+async function deleteRecipients(id = '') {
+  if (!id) {
+    throw new Error('삭제 대상을 지정해주세요.');
+  }
+  const response = await fetch(`${ROOT_URL}/recipients/${id}/`);
+  if (!response.ok) {
+    throw new Error('대상을 삭제하는데 실패했습니다.');
+  }
+  alert('삭제에 성공했습니다.');
 }
